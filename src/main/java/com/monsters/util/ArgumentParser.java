d2f0c48547c0c740e04f3228698278e2dd94bafc
Adam.Stelmach <adam.stelmach@fdmgroup.com>
2022-06-11 14:46:29 +0200

    Adding options v2

--- src/main/java/com/monsters/util/ArgumentParser.java
+++ src/main/java/com/monsters/util/ArgumentParser.java
@@ -4,22 +4,30 @@ import com.monsters.Main;
 import org.apache.commons.cli.*;
 
 public class ArgumentParser {
+
+    String inputPath;
+    String outputPath;
+    int reportNumber;
+
     public static String parseArgs(String[] args) {
         Options options = new Options();
         options.addOption("i", false, "input directory");
         options.addOption("o", false, "output directory");
         options.addOption("h",false,"help");
-        options.addOption("r", true, "report type [1-5]");
+        options.addOption("r", false, "report type [1-5]");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
 
         try {
             cmd = parser.parse(options, args);
-            if (cmd.hasOption("h")) {
+            if (cmd.hasOption("h") || cmd.getOptions().length > 1) {
+                throw new ParseException("Parameter h can only go alone");
+            } else {
                 System.out.println("POMOC");
-            } else if (cmd.hasOption("r")) {
-
+            }
+            if (cmd.hasOption("r")) {
+                String reportType = cmd.getOptionValue("r");
             }
         } catch (ParseException e) {
             HelpFormatter formatter = new HelpFormatter();
