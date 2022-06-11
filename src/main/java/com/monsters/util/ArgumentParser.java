e7c5f0446c8bd3c93f950e2ef060c79781cd7a90
Adam.Stelmach <adam.stelmach@fdmgroup.com>
2022-06-11 14:23:15 +0200

    Adding options

--- src/main/java/com/monsters/util/ArgumentParser.java
+++ src/main/java/com/monsters/util/ArgumentParser.java
@@ -6,11 +6,21 @@ import org.apache.commons.cli.*;
 public class ArgumentParser {
     public static String parseArgs(String[] args) {
         Options options = new Options();
+        options.addOption("i", false, "input directory");
+        options.addOption("o", false, "output directory");
+        options.addOption("h",false,"help");
+        options.addOption("r", true, "report type [1-5]");
 
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
+
         try {
             cmd = parser.parse(options, args);
+            if (cmd.hasOption("h")) {
+                System.out.println("POMOC");
+            } else if (cmd.hasOption("r")) {
+
+            }
         } catch (ParseException e) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("report FILE_PATH", options);
