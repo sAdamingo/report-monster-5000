00ec95e34f885a5617ba3f52bc20fd6cb0f9bb9e
rsumara <rsumara@d17-428-pc-11.ipa.ki.agh.edu.pl>
2022-06-11 11:58:02 +0200

    CLI argument parsed - xls path

--- src/main/java/com/monsters/Main.java
+++ src/main/java/com/monsters/Main.java
@@ -1,4 +1,32 @@
 package com.monsters;
 
+import org.apache.commons.cli.*;
+
 public class Main {
+    public static void main(String[] args) {
+        Options options = new Options();
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("Parsing failed.  Reason: " + e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("report FILE_PATH", options);
+        }
+
+        String xlsPath = null;
+        try {
+            xlsPath = cmd.getArgList().get(0);
+        } catch (Exception e) {
+            System.err.println("Parsing failed.  Reason: " + e.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("report FILE_PATH", options);
+        }
+
+        System.out.println("Searching for xls in path: " + xlsPath);
+
+    }
+
 }
