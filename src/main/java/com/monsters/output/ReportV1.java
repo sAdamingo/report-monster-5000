3dfbd96bee06c5c6624cf8166b2bba52e059997a
pwieczer <pjfw78@gmail.com>
2022-06-11 14:26:43 +0200

    print to console with formatting

--- src/main/java/com/monsters/output/ReportV1.java
+++ src/main/java/com/monsters/output/ReportV1.java
@@ -8,41 +8,46 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-public class ReportV1 implements Report{
+public class ReportV1 implements Report {
     List<Entry> entryList;
 
     public ReportV1(List<Entry> entryList) {
         this.entryList = entryList;
     }
 
-    HashMap<String, Double> sumEntryList(List<Entry> entryList){
-        HashMap<String, Double>summedEntries = new HashMap<>();
-        for (Entry entry :entryList ) {
-            if(summedEntries.containsKey(entry.getUser())) {
+    HashMap<String, Double> sumEntryList(List<Entry> entryList) {
+        HashMap<String, Double> summedEntries = new HashMap<>();
+        for (Entry entry : entryList) {
+            if (summedEntries.containsKey(entry.getUser())) {
                 Double sum = summedEntries.get(entry.getUser());
-                summedEntries.put(entry.getUser(), sum+entry.getDuration());
-            }else {
+                summedEntries.put(entry.getUser(), sum + entry.getDuration());
+            } else {
                 summedEntries.put(entry.getUser(), entry.getDuration());
             }
         }
         return summedEntries;
     }
 
-    String printHashMap(HashMap<String, Double>summedEntries){
+    String printHashMap(HashMap<String, Double> summedEntries) {
         String keyHeader = "Imie Nazwisko";
-//        int maxKey = keyHeader.length();
+        int maxKey = keyHeader.length();
         String valueHeader = "Liczba godzin";
-//        int maxValue = valueHeader.length();
-//
-//        for (String element:summedEntries.keySet()) {
-//            element.
-//        }
-        String result = keyHeader +"|"+valueHeader+"\n";
+        int maxValue = valueHeader.length();
 
         for (String element : summedEntries.keySet()) {
-            result = result+element +"|"+summedEntries.get(element).toString()+"\n";
+            if (maxKey < element.length()) {
+                maxKey = element.length();
+            }
+            if (maxValue < summedEntries.get(element).toString().length()) {
+                maxValue = summedEntries.get(element).toString().length();
+            }
         }
-        return  result;
+        String result = (keyHeader + " ".repeat(maxKey)).substring(0,maxKey) + " | " + (valueHeader + " ".repeat(maxValue)).substring(0,maxValue) + "\n";
+
+        for (String element : summedEntries.keySet()) {
+            result = result + (element + " ".repeat(maxKey)).substring(0,maxKey) + " | " + (summedEntries.get(element).toString() + " ".repeat(maxValue)).substring(0,maxValue) + "\n";
+        }
+        return result;
     }
 
     @Override
