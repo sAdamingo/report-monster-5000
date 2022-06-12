2eda849ad372f4f70f57d0ac7b62c2e75cfa23a7
rsumara <rsumara@d17-428-pc-11.ipa.ki.agh.edu.pl>
2022-06-12 14:02:50 +0200

    Revert "Report V5  corrected"
    This reverts commit 07677dfc46f872ab63ef2dcf165ae3d23b7afe5e.

--- src/main/java/com/monsters/output/ReportV5.java
+++ src/main/java/com/monsters/output/ReportV5.java
@@ -66,7 +66,6 @@ public class ReportV5 implements Report {
 
         XYChart chart = new XYChartBuilder().width(800).height(600).title("Projekty").xAxisTitle("X").yAxisTitle("Y").build();
 
-        int i = 0;
         for (Map.Entry<String, List<LocalDate>> entry : xDataHashMapListLocalDate.entrySet()) {
             List<Date> xDataList = new LinkedList<>();
             for (LocalDate item : xDataHashMapListLocalDate.get(entry.getKey())) {
@@ -75,10 +74,9 @@ public class ReportV5 implements Report {
             }
             List<Double> yDataList = new ArrayList<Double>();
             for (Double item : yDataHashMapListDouble.get(entry.getKey())) {
-                yDataList.add(item + yDataHashMapListDouble.get(entry.getKey()).get(i));
+                yDataList.add(item);
             }
             XYSeries series = chart.addSeries(entry.getKey(), xDataList, yDataList);
-            i++;
         }
 
         chart.getStyler().setPlotBackgroundColor(ChartColor.getAWTColor(ChartColor.GREY));
